6126acc06f6cf716e6778c0db2c8af395e595814
thomas210 <thomastabosa10@gmail.com>
2025-02-20 00:06:43 -0300

    code e iniciando ci

---
+++ test_app_soma.py
@@ -0,0 +1,16 @@
+from app_soma import soma
+
+def test_soma():
+    assert soma(2, 3) == 5
+    assert soma(-1, 1) == 0
+    assert soma(0, 0) == 0
+
+def test_soma1():
+    assert soma(2, 3) == 5
+    assert soma(-1, 1) == 0
+    assert soma(0, 0) == 0
+
+def test_soma2():
+    assert soma(2, 3) == 5
+    assert soma(-1, 1) == 0
+    assert soma(0, 0) == 0
